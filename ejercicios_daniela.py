4c045848b4af96ee67fc87c85ab948e59fba12b5
Daneraco92 <raum.arquitectos@gmail.com>
2024-05-07 18:52:07 -0400

    falto la s

--- ejercicios_daniela.py
+++ ejercicios_daniela.py
@@ -1,4 +1,4 @@
-edad_pablito= input('Ingrese su edad mother fucker: ')
+edad_pablito= input('Ingrese su edad mother fuckers: ')
 edad_pablito= int(edad_pablito)
 print(edad_pablito)
 print(type(edad_pablito))
